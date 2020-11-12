7747f9254396289ceedd3e161a2b359c6dd06059
Jonathan <51481339+E5THEEM@users.noreply.github.com>
2020-11-13 02:52:49 +0800

    Revert changes web.php

--- AASClub/routes/web.php
+++ AASClub/routes/web.php
@@ -27,9 +27,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 //User setup
 
 
