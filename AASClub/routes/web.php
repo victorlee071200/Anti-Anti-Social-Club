46323e0299e550020bd3dceadaf0b2eed34d2fbb
victorleezhesheng <36871691+victorleezhesheng@users.noreply.github.com>
2020-11-19 14:53:05 +0800

    Revert "update directory"
    This reverts commit 23b6f4c82a534d10b0945f4336f87d204333f06c.

--- AASClub/routes/web.php
+++ AASClub/routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
- 
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //User setup
@@ -39,7 +39,3 @@ Route::group(['middleware' => ['auth', 'admin']], function(){
 
 
 
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
