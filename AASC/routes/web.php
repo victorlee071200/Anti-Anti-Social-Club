8e9f426d77332821569cd1c2cdf9a84af7be9293
victorleezhesheng <36871691+victorleezhesheng@users.noreply.github.com>
2020-11-20 12:38:21 +0800

    User Login  System

--- AASC/routes/web.php
+++ AASC/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\RegisterController;
 
 /*
@@ -13,8 +14,14 @@ use App\Http\Controllers\Auth\RegisterController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get("/dashboard", [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get("/login", [LoginController::class, 'index'])->name('login');
+Route::post("/login", [LoginController::class, 'store']);
 
 Route::get("/register", [RegisterController::class, 'index'])->name('register');
+Route::post("/register", [RegisterController::class, 'store']);
+
 
 Route::get('/posts', function () {
     return view('posts.index');
