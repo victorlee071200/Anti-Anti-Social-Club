7f8dee319b49b8ba314d2668e0517c8e83dc02bb
victorleezhesheng <36871691+victorleezhesheng@users.noreply.github.com>
2020-11-22 21:52:56 +0800

    Update post_liked.blade.php

--- AASC/resources/views/emails/posts/post_liked.blade.php
+++ AASC/resources/views/emails/posts/post_liked.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Your post was liked
 
-{{$liker->name}}liked one of your posts
+{{$liker->name}} liked one of your posts
 
 @component('mail::button', ['url' => route('posts.show', $post)])
     View post
